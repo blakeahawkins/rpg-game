a8231dd39948c547ca855fbb03e874c6214ea112
blakeahawkins <blakeahawkins@gmail.com>
2018-06-07 23:29:40 -0500

    A little restructuring and beginning gameplay.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,44 @@
 var pikachu = {
-    power: 5,
-    counter: 5,
-    hp: 100
-} = $("#pikachu");
+    power : 5,
+    counter : 5,
+    hp : 100
+};
+
 var zapdos = {
     power: 8,
     counter: 8,
     hp: 120
-} = $("#zapdos");
+};
+
 var articuno = {
     power: 20,
     counter: 20,
     hp: 150
-} = $("#articuno");
+};
+
 var moltres = {
     power: 25,
     counter: 25,
     hp: 180
-} = $("#moltres");
+};
 
-pikachu.hp = $("#pikachu-hp");
-zapdos.hp = $("#zapdos-hp");
-articuno.hp = $("#articuno-hp");
-moltres.hp = $("#moltres-hp");
+$("#pikachu-hp").text(pikachu.hp);
 
-if($("#your-character").is(":empty")) {
-    $("#pikachu").on("click", function() {
-        $("#pikachu").appendTo($("#your-character"))
-    })
-}
+$("#zapdos-hp").text(zapdos.hp);
+
+$("#articuno-hp").text(articuno.hp);
+
+$("#moltres-hp").text(moltres.hp);
+
+$(".active").click(function() {
+    var gameStart = false;
+    return function() {
+        if(!gameStart) {
+            gameStart = true;
+            $("#your-character").append($(this));
+            $(this).removeClass("active");
+            $("#enemies").append($("#players"));
+        }
+    }
+})
+
